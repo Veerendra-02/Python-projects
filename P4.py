392a35bd63ce98b4779df934191984291d04190f
Veerendra-02 <boyaveerendra02@gmail.com>
2026-02-28 09:56:32 +0530

    P4 file added

---
+++ P4.py
@@ -0,0 +1,13 @@
+class Hero:
+    def __init__(self):
+        self.name="Yash"
+        self.age=44
+        self.number=9988776655
+
+    def Act(self):
+        print("Yash is a best Actor")
+
+H=Hero()
+print(H.name)
+print(H.age)
+H.Act()
